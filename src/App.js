998505c68d5c69bcca1ef337ac701f4162ac4af3
Cody Jewell <cjewell468@gmail.com>
2017-07-19 11:00:40 -0400

    Add the Detail component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 
 import Home from "./scenes/Home/Home";
 import ForecastContainer from "./scenes/ForecastContainer/ForecastContainer";
+import Detail from "./scenes/Detail/Detail"
 import Header from "./components/Header/Header";
 
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/forecast" component={ForecastContainer} />
+          <Route path="/detail/:cityName" component={Detail} />
           <Route render={() => <h1>Page Not Found</h1>}/>
         </Switch>
       </div>
---
+++ src/scenes/Detail/Detail.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const DetailWrapper = styled.div`
+    background-color: blue;
+`
+
+const Detail = props => 
+    <DetailWrapper>
+        <h1>DETAILS</h1>
+    </DetailWrapper>
+
+
+
+export default Detail;
